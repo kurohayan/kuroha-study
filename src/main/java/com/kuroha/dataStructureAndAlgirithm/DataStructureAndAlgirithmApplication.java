128de3ee08feb45b407449f40ffb845d356b248b
heiyu <kurohasinya@gmail.com>
2019-11-21 12:32:41 +0800

    初始化项目

---
+++ src/main/java/com/kuroha/dataStructureAndAlgirithm/DataStructureAndAlgirithmApplication.java
@@ -0,0 +1,13 @@
+package com.kuroha.dataStructureAndAlgirithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureAndAlgirithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataStructureAndAlgirithmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kuroha/dataStructureAndAlgirithm/DataStructureAndAlgirithmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kuroha.dataStructureAndAlgirithm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataStructureAndAlgirithmApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
