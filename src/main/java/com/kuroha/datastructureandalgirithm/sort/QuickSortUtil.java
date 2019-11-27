89bee5f0c35169e81404ff904578e6935d7c4bc1
heiyu <kurohasinya@gmail.com>
2019-11-27 10:32:49 +0800

    完善快速排序

--- src/main/java/com/kuroha/datastructureandalgirithm/sort/QuickSortUtil.java
+++ src/main/java/com/kuroha/datastructureandalgirithm/sort/QuickSortUtil.java
@@ -1,5 +1,7 @@
 package com.kuroha.datastructureandalgirithm.sort;
 
+import com.alibaba.fastjson.JSON;
+
 /**
  * 快速排序
  * @author kuroha
@@ -7,7 +9,42 @@ package com.kuroha.datastructureandalgirithm.sort;
 public class QuickSortUtil {
 
     public static void sort(int[] data) {
+        quick_sort(data,0,data.length-1);
+    }
 
+    public static void quick_sort(int[] data,int start,int end) {
+        if (start >= end) {
+            return;
+        }
+        int size = end-start + 1;
+        int p = data[end];
+        int j = 0;
+        int k = 0;
+        int[] data1 = new int[size];
+        int[] data2 = new int[size];
+        for (int i = start; i < end; i++) {
+            if (data[i] >= p) {
+                data2[k++] = data[i];
+            }else {
+                data1[j++] = data[i];
+            }
+        }
+        quick_sort(data1, 0, j - 1);
+        quick_sort(data2, 0, k - 1);
+        int i = start;
+        for (int l = 0; l < k; l++) {
+            data[i++] = data2[l];
+        }
+        data[i++] = p;
+        for (int l = 0; l < j; l++) {
+            data[i++] = data1[l];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] data = new int[] {4,2,6,3,1,3,6,3,5,7,83,523,44,53};
+        sort(data);
+        System.out.println(JSON.toJSONString(data));
     }
 
 }
