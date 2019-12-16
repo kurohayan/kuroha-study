158b842f7c0922fdd64910b85fcbff713c651920
heiyu <kurohasinya@gmail.com>
2019-12-16 18:06:58 +0800

    暂存

---
+++ src/main/java/com/kuroha/datastructureandalgirithm/search/BinarySearch1.java
@@ -0,0 +1,12 @@
+package com.kuroha.datastructureandalgirithm.search;
+
+/**
+ * @author kuroha
+ */
+public class BinarySearch1 {
+
+    public int search(int[] data, int num) {
+        return 0;
+    }
+
+}
